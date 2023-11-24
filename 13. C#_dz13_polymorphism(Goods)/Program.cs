3963cf0673ce1dfefe3a4206487331487e6cf43a
Vetal <vvvv@vvvv.vv>
2023-11-24 19:12:02 +0300

    1

--- 13. C#_dz13_polymorphism(Goods)/Program.cs
+++ 13. C#_dz13_polymorphism(Goods)/Program.cs
@@ -10,13 +10,13 @@ namespace _13.C__dz13_polymorphism_Goods_
 	{
 		static void Main(string[] args)
 		{
-			Bread bread_1 = new Bread("Хлеб", 30.5, 10, DateTime.Now, DateTime.Now, "Ржаной");
+			Bread bread_1 = new Bread("Хлеб", 30.5, 10, DateTime.Now, new DateTime(2023,11,25), "Ржаной");
 			bread_1.Print();
 			bread_1.AddGoods(10);
 			bread_1.Print();
 			bread_1.SellGoods(2);
 			bread_1.Print();
-			Car car_1 = new Car ("Машина", 10000000, 10, DateTime.Now, DateTime.Now, "Порше Кайен");
+			Car car_1 = new Car ("Машина", 10000000, 10, DateTime.Now, new DateTime(2025,11,30), "Порше Кайен");
 			car_1.SellGoods();
 			car_1.Print();
 
